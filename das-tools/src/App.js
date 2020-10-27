a8b4002e7e8625192f02a67baa4ae997aafa069f
ProRoger <roger.e.cerpa@gmail.com>
2020-10-26 21:03:30 -0400

    updated homepage layout

--- das-tools/src/App.js
+++ das-tools/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import MainHeader from "./components/MainHeader/MainHeader"
 import Grid from '@material-ui/core/Grid';
 import HomePage from "./components/MainContent/HomePage/HomePage"
+import { Container } from '@material-ui/core';
 
 
 
@@ -12,8 +13,23 @@ function App() {
     
       <div className="App">
       <Grid container spacing={3}>
-        <Grid item xs={12}><MainHeader/></Grid>
-        <HomePage/>
+        <Grid item xs={12}>
+          <MainHeader/>
+        </Grid>
+
+        <Container>
+          <Grid item xs={12}>
+            <HomePage/>
+          </Grid>
+          <Grid item xs={12}>
+            <HomePage/>
+          </Grid>
+          <Grid item xs={12}>
+            <HomePage/>
+          </Grid>
+        </Container>
+        
+        
       </Grid>
       </div>
     
--- das-tools/src/components/MainContent/HomePage/HomePage.jsx
+++ das-tools/src/components/MainContent/HomePage/HomePage.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
-import Link from '@material-ui/core/Link';
-import TriageHeader from "../TriageTool/TriageHeader/TriageHeader"
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
-function HomePage() {
+export default function CenteredGrid() {
+  const classes = useStyles();
 
-    const preventDefault = (event) => event.preventDefault();
-    return (
-        <div>
-            <Link href={TriageHeader} onClick={preventDefault}>TriageTools</Link>
-        </div>
-    )
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
-
-export default HomePage
